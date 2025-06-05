1d9076c9e7bf2c5cd98f058329c74f44a2005401
Samy Bouma Ngock <11301943+sabank@users.noreply.github.com>
2025-06-05 13:02:10 -0400

    update index.html carousel with swiper.js

---
+++ assets/js/swiper-init.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
