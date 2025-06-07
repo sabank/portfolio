a7ea9dca3ec6dc8aa33a371898f4c87b7f7d13b6
Samy Bouma Ngock <11301943+sabank@users.noreply.github.com>
2025-06-07 14:55:26 -0400

    refactor html, css, js for carousel

--- assets/js/swiper-init.js
+++ assets/js/swiper-init.js
@@ -1,7 +1,9 @@
+
 const swiper = new Swiper(".mySwiper", {
   loop: true,
-  spaceBetween: 20,
   centeredSlides: true,
+  slidesPerView: 1.2,
+  spaceBetween: 30,
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
@@ -15,4 +17,12 @@ const swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    768: {
+      slidesPerView: 1.5,
+    },
+    1024: {
+      slidesPerView: 2.2,
+    }
+  }
 });
